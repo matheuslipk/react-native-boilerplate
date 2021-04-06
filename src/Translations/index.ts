6c67065dc499a9ca743d6548eca478c0e4e07fb2
Matheus Araujo <matheus.dev3000@gmail.com>
2021-04-06 20:40:12 -0300

    Add translations

---
+++ src/Translations/index.ts
@@ -0,0 +1,14 @@
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import en from './en';
+import pt from './pt';
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en,
+    pt,
+  },
+  lng: 'en',
+});
+
+export default i18n;
---
+++ src/Translations/pt.ts
@@ -0,0 +1,7 @@
+export default {
+  translation: {
+    welcome: 'Bem vindo ao boilerplate',
+    goToHome: 'Ir para home',
+    enter: 'Entrar',
+  },
+};
